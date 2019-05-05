d829310b3329fcf9a14293ce5719904ade25f160
KirkNovos <43241861+KirkNovos@users.noreply.github.com>
2019-05-05 22:26:47 +0300

    Add files via upload

---
+++ kursovaya_4_kurs/Graph_ver1.0/Graph_ver1.0/Unit1.h
@@ -0,0 +1,40 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit1H
+#define Unit1H
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+#include <Chart.hpp>
+#include <DbChart.hpp>
+#include <Dialogs.hpp>
+#include <ExtCtrls.hpp>
+#include <TeEngine.hpp>
+#include <TeeProcs.hpp>
+#include <Series.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+        TButton *Button1;
+        TOpenDialog *OpenDialog1;
+        TButton *Button2;
+        TChart *Chart1;
+        TColorBox *ColorBox1;
+        TEdit *Edit1;
+        TLabel *Label1;
+        TLabel *Label2;
+        TButton *Button3;
+        void __fastcall Button1Click(TObject *Sender);
+        void __fastcall Button2Click(TObject *Sender);
+        void __fastcall Button3Click(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+        __fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
